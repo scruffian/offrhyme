bea6020d6dc24f5835456eaf2683baf06b7ae13d
Ben Dwyer <ben@scruffian.com>
2025-12-24 16:24:23 +1300

    make it work offline

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'offrhyme-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/spoons.html',
+  '/words.js',
+  '/rhymes.js',
+  '/phoneticRhymes.js',
+  '/rhyme-table.js',
+  '/top-fifty.js',
+  '/wordFrequency.js'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache first, then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
