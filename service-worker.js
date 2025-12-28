9bb5d6c7b03da976e3e5759b115af7041e94c276
Ben Dwyer <ben@scruffian.com>
2025-12-29 12:26:33 +1300

    scroll to top

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'offrhyme-v87';
+const CACHE_NAME = 'offrhyme-v98';
 const urlsToCache = [
   './',
   './index.html',
@@ -20,6 +20,8 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
 });
 
 // Fetch from cache first, then network
@@ -49,6 +51,9 @@ self.addEventListener('activate', event => {
           }
         })
       );
+    }).then(() => {
+      // Take control of all pages immediately
+      return self.clients.claim();
     })
   );
 });
