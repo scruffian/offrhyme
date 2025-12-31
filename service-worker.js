3a4fcb1194fce1d3ef9e172dbacedbc21dbf0305
Ben Dwyer <ben@scruffian.com>
2025-12-31 18:59:11 +1300

    centralize css

--- service-worker.js
+++ service-worker.js
@@ -1,62 +1,63 @@
-const CACHE_NAME = 'offrhyme-v123';
+const CACHE_NAME = "offrhyme-v123";
 const urlsToCache = [
-  './',
-  './index.html',
-  './spoons.html',
-  './logo.svg',
-  './icon-192.png',
-  './icon-512.png',
-  './words.js',
-  './rhymes.js',
-  './phoneticRhymes.js',
-  './rhyme-table.js',
-  './top-fifty.js',
-  './wordFrequency.js'
+  "./",
+  "./index.html",
+  "./spoons.html",
+  "./styles.css",
+  "./logo.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./words.js",
+  "./rhymes.js",
+  "./phoneticRhymes.js",
+  "./rhyme-table.js",
+  "./top-fifty.js",
+  "./wordFrequency.js",
 ];
 
 // Install service worker and cache assets
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
   // Force the waiting service worker to become the active service worker
   self.skipWaiting();
 });
 
 // Fetch from cache first, then network
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
-    )
+      return fetch(event.request);
+    })
   );
 });
 
 // Clean up old caches
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      // Take control of all pages immediately
-      return self.clients.claim();
-    })
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => {
+        // Take control of all pages immediately
+        return self.clients.claim();
+      })
   );
 });
